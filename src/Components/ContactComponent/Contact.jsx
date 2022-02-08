d8946d44edc769e9a569ea5ba845f9c594c7d009
Zachary Fisher <zacharyfisher808@gmail.com>
2022-02-07 18:17:54 -1000

    Updated Contact Component

--- src/Components/ContactComponent/Contact.jsx
+++ src/Components/ContactComponent/Contact.jsx
@@ -5,15 +5,17 @@ import { Container, Row, Col } from 'react-bootstrap';
 const Contact = () => {
     return (
         <Container className="contactContainer" fluid>
-            <Row>
+            <Row className="contactRow">
                 <Col xs={12}>
-                    <h1>Follow For More!</h1>
+                    <h1 className="contactTitle">Follow For More!</h1>
                 </Col>
-                <Col className="contactLinks" xs={6}>
-                    <h1>This will be a link to Insta</h1>
+                <Col xs={6}>
+                    <a href="https://www.instagram.com/chura_photography/?hl=en" className="contactInsta" target="_blank" rel="noreferrer"><i className="fab fa-instagram contactIcon"></i></a>
+                    <div><a href="https://www.instagram.com/chura_photography/?hl=en" className="instaCaption">Instagram</a ></div>
                 </Col>
-                <Col className="contactLinks" xs={6}>
-                    <h1>This will be a link to Email</h1>
+                <Col xs={6}>
+                    <a href="https://www.instagram.com/chura_photography/?hl=en" className="contactMail" target="_blank" rel="noreferrer"><i className="fas fa-envelope-open contactIcon"></i></a>
+                    <div><a href="https://www.instagram.com/chura_photography/?hl=en" className="mailCaption">Mail</a ></div>
                 </Col>
             </Row>
         </Container>
