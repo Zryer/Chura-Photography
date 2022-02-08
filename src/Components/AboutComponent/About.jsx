4befc160ef7c6c79b8a9d4f04a710ffd64a0dd78
Zachary Fisher <zacharyfisher808@gmail.com>
2022-02-07 18:42:57 -1000

    Fixed about

--- src/Components/AboutComponent/About.jsx
+++ src/Components/AboutComponent/About.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Sakura from '../../Images/sakuraHome.png';
 
 const About = () => {
     return (
         <Container className="aboutContainer" fluid>
             <Row className="aboutRow">
-                <Col className="p-0 aboutCol">
-                    <img src={Sakura} className="aboutImage" />
+                <Col className="aboutCol">
+                    <img src={Sakura} alt="Sakura" className="aboutImage" />
                 </Col>
-                <Col className="p-0 aboutCol">
-                    <h1 className="aboutContent noPadding">Hello I'm Sakura</h1>
+                <Col className=" aboutCol">
+                    <h1 className="aboutContent">Hello I'm Sakura</h1>
                 </Col>
             </Row>
         </Container>
