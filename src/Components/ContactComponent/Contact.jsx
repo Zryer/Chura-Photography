f01d73687235289b485ee02c08a3ba0f8eda0cd6
Zachary Fisher <zacharyfisher808@gmail.com>
2022-02-13 18:31:51 -1000

    navbar color change need more work for contact

--- src/Components/ContactComponent/Contact.jsx
+++ src/Components/ContactComponent/Contact.jsx
@@ -11,11 +11,11 @@ const Contact = () => {
                 </Col>
                 <Col xs={6}>
                     <a href="https://www.instagram.com/chura_photography/?hl=en" className="contactInsta" target="_blank" rel="noreferrer"><i className="fab fa-instagram contactIcon"></i></a>
-                    <div><a href="https://www.instagram.com/chura_photography/?hl=en" className="instaCaption">Instagram</a ></div>
+                    <div><a href="https://www.instagram.com/chura_photography/?hl=en" className="instaCaption">Instagram</a></div>
                 </Col>
                 <Col xs={6}>
                     <a href="https://www.instagram.com/chura_photography/?hl=en" className="contactMail" target="_blank" rel="noreferrer"><i className="fas fa-envelope-open contactIcon"></i></a>
-                    <div><a href="https://www.instagram.com/chura_photography/?hl=en" className="mailCaption">Mail</a ></div>
+                    <div><a href="https://www.instagram.com/chura_photography/?hl=en" className="mailCaption">Mail</a></div>
                 </Col>
             </Row>
         </Container>
