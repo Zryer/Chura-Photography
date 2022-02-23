17d6d8c6aa8a61b1bc2d68c2268de11fca31cfae
Zachary Fisher <zacharyfisher808@gmail.com>
2022-02-22 17:55:37 -1000

    About Page

--- src/Components/AboutComponent/About.jsx
+++ src/Components/AboutComponent/About.jsx
@@ -6,30 +6,30 @@ import Navi from '../NavbarComponent/Navbar';
 const About = () => {
     return (
         <>
-        <Navi/>
-        <Container className="aboutContainer" fluid>
-            <Row className="aboutRow">
-                <Col className="aboutCol">
-                    <img src={Sakura} alt="Sakura" className="aboutImage" />
-                </Col>
-            </Row>
-            <Row className="aboutRow">
-            <Col className="aboutCol" xs={6}>
-                    <h1 className="aboutContent">Hello</h1>
-                </Col>
-                <Col className="aboutCol">
-                    <h1 className="aboutContent">Hello</h1>
-                </Col>
-            </Row>
-            <Row className="aboutRow">
-            <Col className="aboutCol">
-                    <h1>Hello</h1>
-                </Col>
-                <Col className="aboutCol">
-                    <h1>Hello</h1>
-                </Col>
-            </Row>
-        </Container>
+            <Navi />
+            <Container className="aboutContainer" fluid>
+                <Row>
+                    <Col className="aboutCol">
+                        <img src={Sakura} alt="Sakura" className="aboutImage" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="aboutCol" xs={6}>
+                        <h1 className="aboutContent">Image</h1>
+                    </Col>
+                    <Col className="aboutCol">
+                        <h1 className="aboutContent">Biography</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="aboutCol">
+                        <h1 className="aboutContent">Plans for the future</h1>
+                    </Col>
+                    <Col className="aboutCol">
+                        <h1 className="aboutContent">Image</h1>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
